0e1ec264a7c59b87a90c6c173bce8cefdd183a7d
Ossi Karjalainen <t8kaos00@students.oamk.fi>
2020-03-11 20:40:36 +0200

    Fixing NavigationContainer

--- components/NotLoggedComponents/NotLoggedHeader.js
+++ components/NotLoggedComponents/NotLoggedHeader.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native'
 import Login from '../LoginComponents/Login';
+import Searchi from '../SearchComponents/Search';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 
 const Stack = createStackNavigator();
 
@@ -10,13 +12,14 @@ export default class NotLoggedHeader extends React.Component {
 
     render(){
         return (
-          
-            <NavigationContainer style={styles.container}>
-                <Stack.Navigator initialRouteName="Login">
-                    <Stack.Screen name="Login" component={Login} />
+          <View style={ styles.container }>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Login" component={Login}/>
+                    <Stack.Screen name="Searchi" component={Searchi}/>
                 </Stack.Navigator>
             </NavigationContainer>
-          
+          </View>
            // <Login apiURI='http://85.23.62.54:3000'></Login>
           
         );
@@ -27,8 +30,6 @@ export default class NotLoggedHeader extends React.Component {
         container: {
             flex: 1,
             backgroundColor: '#fff',
-            width: '100%',
-            alignItems: 'center',
-            justifyContent: 'center',
+            width: '100%'
         },
 });
--- components/SearchComponents/Search.js
+++ components/SearchComponents/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
 import { SearchBar } from 'react-native-elements';
 
 export default class Search extends React.Component {
@@ -14,11 +15,12 @@ export default class Search extends React.Component {
     const { search } = this.state;
 
     return (
+      <View>
       <SearchBar
         placeholder="Type Here..."
         onChangeText={this.updateSearch}
         value={search}
-      />
+      /></View>
     );
   }
 }
