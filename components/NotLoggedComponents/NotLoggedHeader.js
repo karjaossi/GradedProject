55ad8242e33c7a3b6535ba50996d43cb54f3d8f2
Ossi Karjalainen <t8kaos00@students.oamk.fi>
2020-03-11 19:16:05 +0200

    Just random stuff

--- components/NotLoggedComponents/NotLoggedHeader.js
+++ components/NotLoggedComponents/NotLoggedHeader.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native'
 import Login from '../LoginComponents/Login';
-import { Header } from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 
 export default class NotLoggedHeader extends React.Component {
 
     render(){
         return (
-            <Header
-            leftComponent={{ icon: 'menu', color: '#fff' }}
-            centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-            rightComponent={{ icon: 'home', color: '#fff' }}/>
-          <View style={styles.container}>
-            <Login apiURI='http://85.23.62.54:3000'></Login>
-          </View>
+          
+            <NavigationContainer style={styles.container}>
+                <Stack.Navigator initialRouteName="Login">
+                    <Stack.Screen name="Login" component={Login} />
+                </Stack.Navigator>
+            </NavigationContainer>
+          
+           // <Login apiURI='http://85.23.62.54:3000'></Login>
+          
         );
       }
     }
